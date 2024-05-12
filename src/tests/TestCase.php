bf0c416b3ab50739c46642c214405fc59d123fe9
Henrique Albuquerque <henrique-albuquerque@hotmail.com>
2024-05-11 21:56:56 -0300

    feat(consulta-cep): Configuração e criação de testes com PHPUNIT

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,9 +2,14 @@
 
 namespace Tests;
 
+use App\Models\Endereco;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function createEndereco(array $options = []) {
+        return Endereco::factory()->create($options);
+    }
 }
